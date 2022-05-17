51ee01ac43439576ddd8abe053cffcc8165ddb84
hoangkhoachau <21120481@student.hcmus.edu.vn>
2022-05-17 11:05:20 +0700

    11h05

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 using namespace std;
-int main(void)
-{
-     string data;           // file contains an undermined number of integer values
-     ifstream fin;     // declare stream variable name
 
-     fin.open("test.txt",ios::in);    // open file   
-     fin >> data;        // get first number from the file (priming the input statement)
-                              // You must attempt to read info prior to an eof( ) test.
-     while (!fin.eof( ))      //if not at end of file, continue reading numbers
-     {
-          cout<<data<<endl;    //print numbers to screen
-          fin >> data;               //get next number from file
-     }
-     fin.close( );       //close file
-     return 0;
+int main(){
+    cout << "[0;33;5;40;100mt~";
+    return 0;
 }
